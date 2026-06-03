be6f88055fc23ca5c9e48f05150243bec7d77be3
gattini0928 <gabrielgattini659@gmail.com>
2026-06-03 16:22:02 -0300

    val

--- internal/services/validators/age.go
+++ internal/services/validators/age.go
@@ -6,7 +6,7 @@ import (
 
 func ValidateAge(age int, role string) error {
 	if age < 21 {
-		return errors.New("Você precisa ser maior de 21 anos para exercer a função de terapeuta")
+		return errors.New("Você precisa ser maior de 21 anos para exercer a função de terapeuta ou psiquiatra")
 	}
 
 	if age > 90 {
